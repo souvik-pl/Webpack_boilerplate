131e0bd32862c3e47dabf413f18e8f3680f0a1d0
paul-souvik <souvik@zaimler.io>
2024-02-04 21:10:55 +0530

    Empty webpack project initialized reference1: https://blog.logrocket.com/using-webpack-typescript/ reference2: https://webpack.js.org/concepts/

---
+++ README.md
@@ -0,0 +1,8 @@
+# Workbench
+
+## Local Setup
+1. Pull this branch into your system.
+2. Make sure you have Node installed (preferrably Node v18.17.1).
+3. Navigate to the project directory and run `npm install`.
+4. Once all the dependencies have been installed, spin up the local server with `npm run start` command. This command will also enable live reloading feature.
+5. For production build, use `npm run build` command. It will create a **dist** folder.
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Workbench",
+      template: "src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 4000,
+  },
+};
